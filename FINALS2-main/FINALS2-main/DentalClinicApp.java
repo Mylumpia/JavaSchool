0899a8b5b1ccc4f724888f56a5fddef819d86f8c
Mylumpia <capkeith43@gmail.com>
2025-06-15 03:56:54 +0800

    Add files via upload

---
+++ FINALS2-main/FINALS2-main/DentalClinicApp.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class DentalClinicApp {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            try {
+                DatabaseConnector.initializeDatabase();
+                new MainFrame().setVisible(true);
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, 
+                    "Application failed to start: " + e.getMessage(), 
+                    "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        });
+    }
+}
